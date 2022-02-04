09da749d3fba307945736b979d8aeb7a0dc56e52
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2022-02-04 21:57:37 +0530

    twick some things

--- App/website/src/Components/Work/Work.jsx
+++ App/website/src/Components/Work/Work.jsx
@@ -24,7 +24,7 @@ export default function Work() {
 
 
     return (
-      <div className='w-full flow-root h-screen lg:mt-30'>
+      <div className='w-full flow-root h-screen lg:mt-20'>
         <div className="relative text-txt-primary-dark-bg flex flex-col w-full justify-between items-center 400:mt-32  mx-auto z-0 sm:flex-row sm:items-center sm:justify-between sm:w-10/12 lg:w-8/12">
           <h1 className={` custom-animation ${searchComp && "active"} font-primaryHeading relative before:content-[''] before:absolute before:-bottom-2 before:left-1/2 before:transform before:-translate-x-2/4 before:w-11/12 max-w-16rem before:h-1 before:bg-background-secondary-accent-color before:rounded-full text-base leading-5 text-center mb-8  normal:text-lg normal:before:w-full sm:text-2xl sm:before:h-2 sm:before:-bottom-4 sm:before:left-60 sm:mb-0 sm:max-w-128`}>Project, that I've worked on -</h1>
                 <div className="actions flex">
