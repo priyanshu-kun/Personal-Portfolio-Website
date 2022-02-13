dadfd6a2b6db05ef3d9f969b5a1d5714915655fd
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2022-02-13 23:03:44 +0530

    try to fix project cards

--- App/website/tailwind.config.js
+++ App/website/tailwind.config.js
@@ -1,25 +1,26 @@
 const colors = require('tailwindcss/colors')
 
-module.exports = { 
-  darkMode: 'class',
-  content: [
-      './src/**/*.{js,jsx,ts,tsx}',  
-  ],  
-  theme: {    
-     maxWidth: {
-          '10rem':'10rem',
-          '16rem':'16rem',
-          '128': '36rem',
-          '168': '42rem',
-          '178': '52rem',
-     },
-     minWidth: {
-        '80': '20rem',
-     },
-     minHeight: {
-         '20rem':'20rem'
-     },
-       fontSize: {
+module.exports = {
+   darkMode: 'class',
+   content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+   ],
+   theme: {
+      maxWidth: {
+         '10rem': '10rem',
+         '16rem': '16rem',
+         '128': '36rem',
+         '168': '42rem',
+         '178': '52rem',
+         '76rem': '76rem',
+      },
+      minWidth: {
+         '80': '20rem',
+      },
+      minHeight: {
+         '20rem': '20rem'
+      },
+      fontSize: {
          'super-tiny': '.6rem',
          'tiny': '.8rem',
          'base': '1rem',
@@ -33,73 +34,75 @@ module.exports = {
          '7xl': '4.6rem',
          '8xl': '6rem',
          'fulid': '7vw'
-       },
-     colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'background-primary-dark-color':'#121212',
-        'background-secondary-dark-color':'#141414',
-        'background-primary-light-color':'#FFFDDE',
-        'background-secondary-light-color':'#D9D7F1',
-        'background-primary-accent-color':'#3500d3',
-        'background-secondary-accent-color':'#FF6D42',
-        'txt-primary-dark-bg': colors.white,
-        'txt-secondary-dark-bg':'#d0d0d0',
-        'txt-primary-light-bg': colors.black,
-        'txt-secondary-light-bg':'#474747',
-        'red': colors.red,
-        'success':'#22C55E'
-     },
-     screens: {
-      'tiny':'260px',
-      'normal':'360px',
-      '400':'400px',
-      'fit':'400px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
-    extend: {
-       lineHeight: {
-          '12': '2.8rem',
-          '16': '3.6rem',
-          '18': '4.2rem',
-       },
-       transitionDelay: {
-          '350': '350ms',
-          '800': '800ms',
-       },
-       fontFamily: {
-        bodyText: ['HelveticaNowDisplay-Medium', 'sans-serif'],
-        primaryHeading: ['HelveticaNowDisplay-Black', 'sans-serif'],
-        secondaryHeading: ['HelveticaNowDisplay-Bold', 'sans-serif'],
-        accentFont: ['Nunito','sans-serif'],
       },
-       zIndex: {        
-          'nv-1':'-1',
-          '99999999': '99999999',
-       },
-       height: {
-          '128': '32rem',
-       },
-       width: {
-          '28rem': '30rem',
-          '128': '32rem',
-          '168': '42rem',
-       },
-       transitionTimingFunction: {
-        'bounce': 'cubic-bezier(.8, .5, .2, 1.4)',
+      colors: {
+         transparent: 'transparent',
+         current: 'currentColor',
+         'background-primary-dark-color': '#121212',
+         'background-secondary-dark-color': '#141414',
+         'background-primary-light-color': '#FFFDDE',
+         'background-secondary-light-color': '#D9D7F1',
+         'background-primary-accent-color': '#3500d3',
+         'background-secondary-accent-color': '#FF6D42',
+         'txt-primary-dark-bg': colors.white,
+         'txt-secondary-dark-bg': '#d0d0d0',
+         'txt-primary-light-bg': colors.black,
+         'txt-secondary-light-bg': '#474747',
+         'red': colors.red,
+         'success': '#22C55E'
       },
-      boxShadow: {
-        'activeShadow': '0 0 0 3px rgba(255,255,255, 0.3)',
+      screens: {
+         'tiny': '260px',
+         'normal': '360px',
+         '400': '400px',
+         'fit': '400px',
+         'tempScreen': '550px',
+         'sm': '640px',
+         'md': '768px',
+         'lg': '1024px',
+         'xl': '1280px',
+         '2xl': '1536px',
       },
-       opacity: {
-          'op-primary-60': '.60',
-          'op-secondary-30': '.30',
-       }
-    },  
-  },  
-  plugins: [],
+      extend: {
+         lineHeight: {
+            '12': '2.8rem',
+            '16': '3.6rem',
+            '18': '4.2rem',
+         },
+         transitionDelay: {
+            '350': '350ms',
+            '800': '800ms',
+         },
+         fontFamily: {
+            bodyText: ['HelveticaNowDisplay-Medium', 'sans-serif'],
+            primaryHeading: ['HelveticaNowDisplay-Black', 'sans-serif'],
+            secondaryHeading: ['HelveticaNowDisplay-Bold', 'sans-serif'],
+            accentFont: ['Nunito', 'sans-serif'],
+         },
+         zIndex: {
+            'nv-1': '-1',
+            '99999999': '99999999',
+         },
+         height: {
+            '30rem': '30rem',
+            '128': '32rem',
+         },
+         width: {
+            '28rem': '28rem',
+            '128': '32rem',
+            '168': '42rem',
+         },
+         transitionTimingFunction: {
+            'bounce': 'cubic-bezier(.8, .5, .2, 1.4)',
+         },
+         boxShadow: {
+            'activeShadow': '0 0 0 3px rgba(255,255,255, 0.3)',
+         },
+         opacity: {
+            'op-primary-60': '.60',
+            'op-secondary-30': '.30',
+         }
+      },
+   },
+   plugins: [],
 }
