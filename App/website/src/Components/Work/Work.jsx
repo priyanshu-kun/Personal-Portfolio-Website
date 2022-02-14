e7d7f62b1f820d1ddaf6ff88496db48f23830281
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2022-02-14 23:40:04 +0530

    complete work section

--- App/website/src/Components/Work/Work.jsx
+++ App/website/src/Components/Work/Work.jsx
@@ -45,18 +45,18 @@ export default function Work() {
                 </div>
                 <SearchBar searchComp={searchComp} handleSearchComponent={handleSearchComponent} />
             </div>
-            <div className="projects  w-8/12 mx-auto mt-4 tiny:mt-8 normal:mt-10 fit:mt-14  pb-8 tiny:w-full  tiny:px-4 fit:px-6 sm:px-8 lg:max-w-76rem" >
+            <div className="projects  w-8/12 mx-auto mt-4 tiny:mt-8 normal:mt-10 fit:mt-14 md:mt-20  pb-8 tiny:w-full  tiny:px-4 fit:px-6 sm:px-8 lg:max-w-76rem" >
                 <div onMouseEnter={() => {
                     setCard1(true)
                 }} onMouseLeave={() => {
                     setCard1(false)
                 }} className="w-full h-full  mx-auto relative overflow-hidden">
                     <img className="opacity-80" src={img1} alt="project 1" />
-                    <div className={` ${true ? "projects-overlay": "hide-project-overlay"} absolute top-0 left-0 bottom-0 right-0   flex flex-col justify-between`}>
-                        <h1 className=" tiny:mt-4 tiny:mr-4 normal:mt-6 normal:mr-6 fit:mt-8 fit:mr-8 sm:mt-12 sm:mr-12 md:mt-8 md:mr-8 lg:mt-12 lg:mr-12">01</h1>
-                        <div className="h-full  w-52 ml-6 pt-4 normal:w-72 normal:ml-8 fit:ml-12 tempScreen:w-96 tempScreen:pt-8 tempScreen:ml-12">
-                            <h2 className="font-primaryHeading text-txt-primary-dark-bg/80 text-xl normal:text-3xl tempScreen:text-4xl">Bug Traker - A project to track and solve bugs.</h2>
-                            <p className="text-tiny my-4 text-txt-primary-dark-bg/50 fit:text-base fit:my-6 fit:leading-5 sm:my-8">Reactjs / Nodejs / MongoDB / RestAPI</p>
+                    <div className={` ${card1 ? "projects-overlay": "hide-project-overlay"} absolute top-0 left-0 bottom-0 right-0   flex flex-col justify-between`}>
+                        <h1 className="tiny:mt-4 tiny:mr-4 normal:mt-6 normal:mr-6 fit:mt-8 fit:mr-8 sm:mt-12 sm:mr-12 md:mt-8 md:mr-8 lg:mt-12 lg:mr-12">01</h1>
+                        <div className="h-full  w-52 ml-6 pt-4 normal:w-72 normal:ml-8 fit:ml-12 tempScreen:w-96 tempScreen:pt-8 tempScreen:ml-12 md:w-3/4 md:ml-8 lg:ml-12 lg:w-96 lg:pt-14">
+                            <h2 className="font-primaryHeading text-txt-primary-dark-bg/80 text-xl normal:text-3xl tempScreen:text-4xl md:text-3xl lg:text-4xl">Bug Traker - A project to track and solve bugs.</h2>
+                            <p className="text-tiny my-4 text-txt-primary-dark-bg/60 fit:text-base fit:my-6 fit:leading-5 sm:my-8 md:my-6 md:text-tiny lg:text-base">Reactjs / Nodejs / MongoDB / RestAPI</p>
                             <button className="mr-4 tempScreen:mr-8 cursor-pointer">
                               <IconContext.Provider
                                   value={{ className: 'text-txt-primary-dark-bg opacity-60 w-5 h-5 fit:w-6 fit:h-6 ' }}
@@ -74,62 +74,146 @@ export default function Work() {
                         </div>
                     </div>
                 </div>
-                <div className="w-full h-full mx-auto relative overflow-hidden">
-                    <img className="opacity-80" src={img2} alt="tu tu tu" />
-                    <div className={`projects-overlay absolute top-0 left-0 bottom-0 right-0 hide-project-overlay hover:bg-background-secondary-dark-color/80`}>
-                        <h1>02</h1>
-                        <div className="w-96 mt-20 ml-14">
-                            <h2 className="font-heading text-2xl">Project</h2>
-                            <p className="my-3 text-sm font-secondaryHeading">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, quae reiciendis? Est voluptates nemo, ipsa officiis atque amet harum esse, laudantium explicabo ut, placeat corporis perferendis!</p>
-                            <button className="font-secondaryHeading underline cursor-pointer">Find More -</button>
+                <div onMouseEnter={() => {
+                    setCard2(true)
+                }} onMouseLeave={() => {
+                    setCard2(false)
+                }} className="w-full h-full  mx-auto relative overflow-hidden">
+                    <img className="opacity-80" src={img2} alt="project 1" />
+                    <div className={` ${card2 ? "projects-overlay": "hide-project-overlay"} absolute top-0 left-0 bottom-0 right-0   flex flex-col justify-between`}>
+                        <h1 className="tiny:mt-4 tiny:mr-4 normal:mt-6 normal:mr-6 fit:mt-8 fit:mr-8 sm:mt-12 sm:mr-12 md:mt-8 md:mr-8 lg:mt-12 lg:mr-12">02</h1>
+                        <div className="h-full  w-52 ml-6 pt-4 normal:w-72 normal:ml-8 fit:ml-12 tempScreen:w-96 tempScreen:pt-8 tempScreen:ml-12 md:w-3/4 md:ml-8 lg:ml-12 lg:w-96 lg:pt-14">
+                            <h2 className="font-primaryHeading text-txt-primary-dark-bg/80 text-xl normal:text-3xl tempScreen:text-4xl md:text-3xl lg:text-4xl">Bug Traker - A project to track and solve bugs.</h2>
+                            <p className="text-tiny my-4 text-txt-primary-dark-bg/60 fit:text-base fit:my-6 fit:leading-5 sm:my-8 md:my-6 md:text-tiny lg:text-base">Reactjs / Nodejs / MongoDB / RestAPI</p>
+                            <button className="mr-4 tempScreen:mr-8 cursor-pointer">
+                              <IconContext.Provider
+                                  value={{ className: 'text-txt-primary-dark-bg opacity-60 w-5 h-5 fit:w-6 fit:h-6 ' }}
+                              >
+                                    <FiGithub />
+                              </IconContext.Provider>
+                            </button>
+                            <button className="cursor-pointer">
+                              <IconContext.Provider
+                                value={{ className: 'text-txt-primary-dark-bg opacity-60 w-5 h-5  fit:w-6 fit:h-6' }}
+                              >
+                                    <FiLink />
+                              </IconContext.Provider>
+                            </button>
                         </div>
                     </div>
                 </div>
-                <div className="w-full h-full mx-auto relative overflow-hidden">
-                    <img className="opacity-80" src={img3} alt="project 3" />
-                    <div className={`projects-overlay absolute top-0 left-0 bottom-0 right-0 hide-project-overlay hover:bg-background-secondary-dark-color/80`}>
-                        <h1>03</h1>
-                        <div className="w-96 mt-20 ml-14">
-                            <h2 className="font-heading text-2xl">Project</h2>
-                            <p className="my-3 text-sm font-secondaryHeading">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, quae reiciendis? Est voluptates nemo, ipsa officiis atque amet harum esse, laudantium explicabo ut, placeat corporis perferendis!</p>
-                            <button className="font-secondaryHeading underline cursor-pointer">Find More -</button>
+                <div onMouseEnter={() => {
+                    setCard3(true)
+                }} onMouseLeave={() => {
+                    setCard3(false)
+                }} className="w-full h-full  mx-auto relative overflow-hidden">
+                    <img className="opacity-80" src={img3} alt="project 1" />
+                    <div className={` ${card3 ? "projects-overlay": "hide-project-overlay"} absolute top-0 left-0 bottom-0 right-0   flex flex-col justify-between`}>
+                        <h1 className="tiny:mt-4 tiny:mr-4 normal:mt-6 normal:mr-6 fit:mt-8 fit:mr-8 sm:mt-12 sm:mr-12 md:mt-8 md:mr-8 lg:mt-12 lg:mr-12">03</h1>
+                        <div className="h-full  w-52 ml-6 pt-4 normal:w-72 normal:ml-8 fit:ml-12 tempScreen:w-96 tempScreen:pt-8 tempScreen:ml-12 md:w-3/4 md:ml-8 lg:ml-12 lg:w-96 lg:pt-14">
+                            <h2 className="font-primaryHeading text-txt-primary-dark-bg/80 text-xl normal:text-3xl tempScreen:text-4xl md:text-3xl lg:text-4xl">Bug Traker - A project to track and solve bugs.</h2>
+                            <p className="text-tiny my-4 text-txt-primary-dark-bg/60 fit:text-base fit:my-6 fit:leading-5 sm:my-8 md:my-6 md:text-tiny lg:text-base">Reactjs / Nodejs / MongoDB / RestAPI</p>
+                            <button className="mr-4 tempScreen:mr-8 cursor-pointer">
+                              <IconContext.Provider
+                                  value={{ className: 'text-txt-primary-dark-bg opacity-60 w-5 h-5 fit:w-6 fit:h-6 ' }}
+                              >
+                                    <FiGithub />
+                              </IconContext.Provider>
+                            </button>
+                            <button className="cursor-pointer">
+                              <IconContext.Provider
+                                value={{ className: 'text-txt-primary-dark-bg opacity-60 w-5 h-5  fit:w-6 fit:h-6' }}
+                              >
+                                    <FiLink />
+                              </IconContext.Provider>
+                            </button>
                         </div>
                     </div>
                 </div>
-                <div className=" w-full h-full mx-auto relative overflow-hidden">
-                    <img className="opacity-80" src={img4} alt="project 4" />
-                    <div className={` projects-overlay absolute top-0 left-0 bottom-0 right-0 hide-project-overlay hover:bg-background-secondary-dark-color/80`}>
-                        <h1>04</h1>
-                        <div className="w-96 mt-20 ml-14">
-                            <h2 className="font-heading text-2xl">Project</h2>
-                            <p className="my-3 text-sm font-secondaryHeading">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, quae reiciendis? Est voluptates nemo, ipsa officiis atque amet harum esse, laudantium explicabo ut, placeat corporis perferendis!</p>
-                            <button className="font-secondaryHeading underline cursor-pointer">Find More -</button>
+                <div onMouseEnter={() => {
+                    setCard4(true)
+                }} onMouseLeave={() => {
+                    setCard4(false)
+                }} className="w-full h-full  mx-auto relative overflow-hidden">
+                    <img className="opacity-80" src={img4} alt="project 1" />
+                    <div className={` ${card4 ? "projects-overlay": "hide-project-overlay"} absolute top-0 left-0 bottom-0 right-0   flex flex-col justify-between`}>
+                        <h1 className="tiny:mt-4 tiny:mr-4 normal:mt-6 normal:mr-6 fit:mt-8 fit:mr-8 sm:mt-12 sm:mr-12 md:mt-8 md:mr-8 lg:mt-12 lg:mr-12">04</h1>
+                        <div className="h-full  w-52 ml-6 pt-4 normal:w-72 normal:ml-8 fit:ml-12 tempScreen:w-96 tempScreen:pt-8 tempScreen:ml-12 md:w-3/4 md:ml-8 lg:ml-12 lg:w-96 lg:pt-14">
+                            <h2 className="font-primaryHeading text-txt-primary-dark-bg/80 text-xl normal:text-3xl tempScreen:text-4xl md:text-3xl lg:text-4xl">Bug Traker - A project to track and solve bugs.</h2>
+                            <p className="text-tiny my-4 text-txt-primary-dark-bg/60 fit:text-base fit:my-6 fit:leading-5 sm:my-8 md:my-6 md:text-tiny lg:text-base">Reactjs / Nodejs / MongoDB / RestAPI</p>
+                            <button className="mr-4 tempScreen:mr-8 cursor-pointer">
+                              <IconContext.Provider
+                                  value={{ className: 'text-txt-primary-dark-bg opacity-60 w-5 h-5 fit:w-6 fit:h-6 ' }}
+                              >
+                                    <FiGithub />
+                              </IconContext.Provider>
+                            </button>
+                            <button className="cursor-pointer">
+                              <IconContext.Provider
+                                value={{ className: 'text-txt-primary-dark-bg opacity-60 w-5 h-5  fit:w-6 fit:h-6' }}
+                              >
+                                    <FiLink />
+                              </IconContext.Provider>
+                            </button>
                         </div>
                     </div>
                 </div>
-                <div  className="w-full h-full mx-auto relative overflow-hidden">
-                    <img className="opacity-80" src={img5} alt="project 5" />
-                    <div className={`projects-overlay absolute top-0 left-0 bottom-0 right-0 hide-project-overlay hover:bg-background-secondary-dark-color/80`}>
-                        <h1>05</h1>
-                        <div className="w-96 mt-20 ml-14">
-                            <h2 className="font-heading text-2xl">Project</h2>
-                            <p className="my-3 text-sm font-secondaryHeading">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, quae reiciendis? Est voluptates nemo, ipsa officiis atque amet harum esse, laudantium explicabo ut, placeat corporis perferendis!</p>
-                            <button className="font-secondaryHeading underline cursor-pointer">Find More -</button>
+                <div onMouseEnter={() => {
+                    setCard5(true)
+                }} onMouseLeave={() => {
+                    setCard5(false)
+                }} className="w-full h-full  mx-auto relative overflow-hidden">
+                    <img className="opacity-80" src={img5} alt="project 1" />
+                    <div className={` ${card5 ? "projects-overlay": "hide-project-overlay"} absolute top-0 left-0 bottom-0 right-0   flex flex-col justify-between`}>
+                        <h1 className="tiny:mt-4 tiny:mr-4 normal:mt-6 normal:mr-6 fit:mt-8 fit:mr-8 sm:mt-12 sm:mr-12 md:mt-8 md:mr-8 lg:mt-12 lg:mr-12">05</h1>
+                        <div className="h-full  w-52 ml-6 pt-4 normal:w-72 normal:ml-8 fit:ml-12 tempScreen:w-96 tempScreen:pt-8 tempScreen:ml-12 md:w-3/4 md:ml-8 lg:ml-12 lg:w-96 lg:pt-14">
+                            <h2 className="font-primaryHeading text-txt-primary-dark-bg/80 text-xl normal:text-3xl tempScreen:text-4xl md:text-3xl lg:text-4xl">Bug Traker - A project to track and solve bugs.</h2>
+                            <p className="text-tiny my-4 text-txt-primary-dark-bg/60 fit:text-base fit:my-6 fit:leading-5 sm:my-8 md:my-6 md:text-tiny lg:text-base">Reactjs / Nodejs / MongoDB / RestAPI</p>
+                            <button className="mr-4 tempScreen:mr-8 cursor-pointer">
+                              <IconContext.Provider
+                                  value={{ className: 'text-txt-primary-dark-bg opacity-60 w-5 h-5 fit:w-6 fit:h-6 ' }}
+                              >
+                                    <FiGithub />
+                              </IconContext.Provider>
+                            </button>
+                            <button className="cursor-pointer">
+                              <IconContext.Provider
+                                value={{ className: 'text-txt-primary-dark-bg opacity-60 w-5 h-5  fit:w-6 fit:h-6' }}
+                              >
+                                    <FiLink />
+                              </IconContext.Provider>
+                            </button>
                         </div>
                     </div>
                 </div>
-                <div  className="w-full h-full mx-auto relative overflow-hidden">
-                    <img className="opacity-80" src={img6} alt="project 6" />
-                    <div className={`projects-overlay absolute top-0 left-0 bottom-0 right-0 hide-project-overlay hover:bg-background-secondary-dark-color/80`}>
-                        <h1>06</h1>
-                        <div className="w-96 mt-20 ml-14">
-                            <h2 className="font-heading text-2xl">Project</h2>
-                            <p className="my-3 text-sm font-secondaryHeading">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, quae reiciendis? Est voluptates nemo, ipsa officiis atque amet harum esse, laudantium explicabo ut, placeat corporis perferendis!</p>
-                            <button className="font-secondaryHeading underline cursor-pointer">Find More -</button>
+                <div onMouseEnter={() => {
+                    setCard6(true)
+                }} onMouseLeave={() => {
+                    setCard6(false)
+                }} className="w-full h-full  mx-auto relative overflow-hidden">
+                    <img className="opacity-80" src={img6} alt="project 1" />
+                    <div className={` ${card6 ? "projects-overlay": "hide-project-overlay"} absolute top-0 left-0 bottom-0 right-0   flex flex-col justify-between`}>
+                        <h1 className="tiny:mt-4 tiny:mr-4 normal:mt-6 normal:mr-6 fit:mt-8 fit:mr-8 sm:mt-12 sm:mr-12 md:mt-8 md:mr-8 lg:mt-12 lg:mr-12">06</h1>
+                        <div className="h-full  w-52 ml-6 pt-4 normal:w-72 normal:ml-8 fit:ml-12 tempScreen:w-96 tempScreen:pt-8 tempScreen:ml-12 md:w-3/4 md:ml-8 lg:ml-12 lg:w-96 lg:pt-14">
+                            <h2 className="font-primaryHeading text-txt-primary-dark-bg/80 text-xl normal:text-3xl tempScreen:text-4xl md:text-3xl lg:text-4xl">Bug Traker - A project to track and solve bugs.</h2>
+                            <p className="text-tiny my-4 text-txt-primary-dark-bg/60 fit:text-base fit:my-6 fit:leading-5 sm:my-8 md:my-6 md:text-tiny lg:text-base">Reactjs / Nodejs / MongoDB / RestAPI</p>
+                            <button className="mr-4 tempScreen:mr-8 cursor-pointer">
+                              <IconContext.Provider
+                                  value={{ className: 'text-txt-primary-dark-bg opacity-60 w-5 h-5 fit:w-6 fit:h-6 ' }}
+                              >
+                                    <FiGithub />
+                              </IconContext.Provider>
+                            </button>
+                            <button className="cursor-pointer">
+                              <IconContext.Provider
+                                value={{ className: 'text-txt-primary-dark-bg opacity-60 w-5 h-5  fit:w-6 fit:h-6' }}
+                              >
+                                    <FiLink />
+                              </IconContext.Provider>
+                            </button>
                         </div>
                     </div>
                 </div>
-                
             </div>
       </div>
     )
